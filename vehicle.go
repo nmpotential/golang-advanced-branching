36725fba5e1c4309c16364c87eb0bd708f7f3e78
nmpotential <nathan.mokonz@gmail.com>
2020-08-11 16:43:32 +0100

    Module 1 complete

--- vehicle.go
+++ vehicle.go
@@ -1,15 +1,30 @@
 package main
 
-import (
-
-)
-
-
 // Values array for the feedback.json file
+type Value struct {
+	Models []Model `json:"values"`
+}
+
 
 // Model array for the feedback.json file
+type Model struct {
+	Name string `json:"model"`
+	Feedback []string `json:"feedback"`
+}
 
+type feedbackResult struct {
+	feedbackTotal int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral int
+}
 type rating float32
+
+var (
+	vehicleResult map[string]feedbackResult
+	inventory []vehicle
+)
+
 
 const (
 	extraPositive rating = 1.2
@@ -19,9 +34,28 @@ const (
 	extraNegative rating = -1.2
 )
 
+type vehicle interface {}
+
+type car struct{
+	model string
+	make string
+	typeVehicle string
+}
+
+type truck struct {
+	model string
+	make string
+	typeVehicle string
+}
+
+type bike struct {
+	model string
+	make string
+}
+
 func init() {
 
-/*
+
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,8 +68,8 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
